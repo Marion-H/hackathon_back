3ad9aa646af2a1b46b57d40e31c5fbdf61fe9ee7
Marion-H <doudou6500@gmail.com>
2020-06-24 19:14:25 +0200

    create route put for one client and put for score +1

--- routes/patient.route.js
+++ routes/patient.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const patient = express.Router();
+const sequelize = require("sequelize");
 
 const Patient = require("../sequelize/models/patient.model");
 
@@ -26,26 +27,59 @@ patient.post("/", async (req, res) => {
       birthday,
       score,
     });
-    res.status(201).json(patients)
+    res.status(201).json(patients);
   } catch (err) {
     res.status(422).json({
-        status: "error",
-        message: "invalid request",
-      });
+      status: "error",
+      message: "invalid request",
+    });
   }
 });
 
-patient.get("/:uuid", async (req,res) => {
-    const uuid = req.params.uuid
-    try {
-        const patients = await Patient.findOne({where : {uuid}})
-        res.status(200).json(patients)
-    } catch (err) {
-        res.status(422).json({
-            status: "error",
-            message: "invalid request",
-          });
-    }
-})
+patient.get("/:uuid", async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    const patients = await Patient.findOne({ where: { uuid } });
+    res.status(200).json(patients);
+  } catch (err) {
+    res.status(422).json({
+      status: "error",
+      message: "invalid request",
+    });
+  }
+});
+
+patient.put("/:uuid", async (req, res) => {
+  const uuid = req.params.uuid;
+  const { lastname, firstname, birthday } = req.body;
+  try {
+    await Patient.update(
+      {
+        lastname,
+        firstname,
+        birthday,
+      },
+      { where: { uuid } }
+    );
+    res.status(201).end();
+  } catch (err) {
+    res.status(422).json({
+      status: "error",
+      message: "invalid request",
+    });
+  }
+});
+
+patient.put("/:uuid/click", async (req, res) => {
+  const uuid = req.params.uuid;
+  try {
+    await Patient.update({
+      score: sequelize.literal("score+1"),
+    },{where: {uuid}});
+    res.status(201).end();
+  } catch (err) {
+    res.status(422).json(err);
+  }
+});
 
 module.exports = patient;
